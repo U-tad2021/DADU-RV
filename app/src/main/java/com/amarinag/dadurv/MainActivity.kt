247b7f4b5554f321545839343d0e1858aa57c60b
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-05-11 11:56:53 +0200

    configure RecyclerView

--- app/src/main/java/com/amarinag/dadurv/MainActivity.kt
+++ app/src/main/java/com/amarinag/dadurv/MainActivity.kt
@@ -2,10 +2,18 @@ package com.amarinag.dadurv
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.amarinag.dadurv.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val adapter = DogAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvDogs.adapter = adapter
+        binding.rvDogs.layoutManager = LinearLayoutManager(this)
+        adapter.submitList(dogs)
     }
 }

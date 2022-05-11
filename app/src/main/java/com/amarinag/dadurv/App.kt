e67c0789408582ce508ec037640ea485d022cc0d
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-05-11 09:37:41 +0200

    initial commit

---
+++ app/src/main/java/com/amarinag/dadurv/App.kt
@@ -0,0 +1,32 @@
+package com.amarinag.dadurv
+
+val dogs: List<Dog> = listOf(
+    Dog(
+        "f9fc0564913c4230a976361faa321cd7",
+        "Pancho",
+        "Obediente y tranquilo",
+        15,
+        "https://es.wikipedia.org/wiki/Bulldog#/media/Archivo:Racib%C3%B3rz_2007_082.jpg"
+    ),
+    Dog(
+        "4b24ae4c4cc04c6aa627e0d45867432b",
+        "Bailarín",
+        "Fue reconocido como raza en el siglo XIX, aunque su herencia se remonta a algunos miles de años antes de la de los spaniel originales..",
+        108,
+        "https://es.wikipedia.org/wiki/Cocker_spaniel_ingl%C3%A9s#/media/Archivo:Tihany_English_Cocker_Spaniel_Georg_2.jpg"
+    ),
+    Dog(
+        "8724da83d9b14db8b027b33f6de75c8e",
+        "Cora",
+        "Temperamento equilibrado, con nervios firmes, seguro de sí mismo, desenvuelto y (excepto cuando está provocado) amigable",
+        36,
+        "https://en.wikipedia.org/wiki/German_Shepherd#/media/File:German_Shepherd_-_DSC_0346_(10096362833).jpg"
+    ),
+    Dog(
+        "df36bf1ef1614dcd849cadb772d2348c",
+        "Percha",
+        "El Persa es una raza de gato caracterizada por tener una cara ancha y plana y un gran abundante pelaje de variados colores",
+        8,
+        "https://es.wikipedia.org/wiki/Gato_persa#/media/Archivo:Persian_in_Cat_Cafe.jpg"
+    ),
+)
---
+++ app/src/main/java/com/amarinag/dadurv/Dog.kt
@@ -0,0 +1,9 @@
+package com.amarinag.dadurv
+
+data class Dog(
+    val id: String,
+    val name: String,
+    val description: String,
+    val age: Int,
+    val imageUrl: String
+)

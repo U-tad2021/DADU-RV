a4bac4805f7ec357926b68184e304afb77b99439
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-05-11 12:09:19 +0200

    create DetailActivity.kt and added ViewBinding

---
+++ app/src/main/java/com/amarinag/dadurv/DetailActivity.kt
@@ -0,0 +1,14 @@
+package com.amarinag.dadurv
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.amarinag.dadurv.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding)
+    }
+}

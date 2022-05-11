54ddf1b0b8c00e0583b60a4e330fba6dd51c3041
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-05-11 11:10:53 +0200

    Added ViewHolder

---
+++ app/src/main/java/com/amarinag/dadurv/DogAdapter.kt
@@ -0,0 +1,31 @@
+package com.amarinag.dadurv
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class DogAdapter : ListAdapter<Dog, DogAdapter.DogViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_dog, parent, false)
+        return DogViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    class DogViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+}
+
+class DogItemCallback : DiffUtil.ItemCallback<Dog>() {
+    override fun areItemsTheSame(oldItem: Dog, newItem: Dog): Boolean = oldItem == newItem
+
+    override fun areContentsTheSame(oldItem: Dog, newItem: Dog): Boolean = oldItem == newItem
+}
